797a329f61681d95188b69a1ac22e80ca32b5d57
Jeremy Munsell <jeremy.munsell.2022@gmail.com>
2022-10-17 20:51:00 -0500

    Added json-server

--- src/App.js
+++ src/App.js
@@ -7,25 +7,7 @@ import { useState } from "react"
 const App = () => {
   const [showAddTask,setShowAddTask] = useState(false)
   const [tasks,setTasks] = useState([
-//     {
-//         id: 1,
-//         text: 'Doctors Appt',
-//         day: 'Feb 5th at 2:30',
-//         reminder: true
-
-// },
-// {
-//     id: 2,
-//     text: 'Meeting at School',
-//     day: 'Feb 6th at 1:30pm',
-//     reminder: true
-// },
-// {
-//     id: 3,
-//     text: 'Food Shopping',
-//     day: 'Feb 5th at 2:30 pm',
-//     reminder: true
-// }
+// 
 ])
 
 //Delete Taks
